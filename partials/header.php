0dad44c17ad0d612c9e241d76d04fd76aaaa61d7
Mitake <kev800da@gmail.com>
2024-03-22 11:18:52 +0100

    Fixe des lien dans le header, et des problème avec le common.php

--- partials/header.php
+++ partials/header.php
@@ -1,11 +1,11 @@
 <div class="header">
 <a href="#default" class="logo">La pdp de quelqu'un</a>
     <div class="header-right">
-        <a href="">Accueil</a>
-        <a href="">Connexion</a>
-        <a href="">Inscription</a>
-        <a href="">Regles</a>
-        <a href="">Jeu</a>
-        <a href="">Profil</a>
+        <a href="<?php SITE_ROOT  ?> pageAccueil.php">Accueil</a>
+        <a href="<?php SITE_ROOT  ?> pageConnexion.php">Connexion</a>
+        <a href="<?php SITE_ROOT  ?> pageInscription.php">Inscription</a>
+        <a href="<?php SITE_ROOT  ?> rules.php">Regles</a>
+        <a href="<?php SITE_ROOT  ?> pageAttente.php">Jeu</a>
+        <a href="<?php SITE_ROOT  ?> pageProfil.php">Profil</a>
     </div>
 </div>
