84fad19138c70055441c9df31b39ad469d6a6c4e
Mitake <kev800da@gmail.com>
2024-03-27 10:04:44 +0100

    test1

--- pageInscription.php
+++ pageInscription.php
@@ -1,7 +1,9 @@
 <?php
 require_once './utils/common.php';
+require_once './utils/database.php';
+$pdo = connectToDbAndGetPdo();
 ?>
-<!DOCTYPE html>
+<!DOCTYPE html >
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
@@ -12,32 +14,78 @@ require_once './utils/common.php';
 </head>
 <body>
   <?php include 'partials/header.php'; ?>
-  <div class="conteneurFormulaire">
+  <div class="connexion">
   <?php
-  if(isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    if (!empty($_POST)) {
 
-    // Base de données
-    $db = new PDO('mysql:host=localhost;dbname=nom_de_la_base_de_donnees', 'utilisateur', 'mot_de_passe');
+        // Vérification de l'email
+        if (isset($_POST['mail'])) {
+            if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+                $error_message_mail = "Le format de l'email n'est pas valide";
+            } else {
+                $pdoStatement = $pdo->prepare('SELECT mail FROM `user` WHERE mail = :mail');
+                $pdoStatement->execute([':mail' => $_POST['mail']]);
+                $existing_mail = $pdoStatement->fetchColumn();
+                if ($existing_mail) {
+                    $error_message_mail = 'L\'adresse email existe déjà';
+                }
+            }
+        }
 
-    $stmt = $db->prepare('INSERT INTO utilisateurs (username, email, password) VALUES (?, ?, ?)');
-    $stmt->execute([$username, $email, $password]);
+        // Vérification du pseudo
+        if (isset($_POST['nom'])) {
+            if (strlen($_POST['nom']) < 4) {
+                $error_message_pseudo = 'Le pseudo doit comporter au moins 4 caractères';
+            } else {
+                $pdoStatement = $pdo->prepare('SELECT nom FROM `user` WHERE nom = :nom');
+                $pdoStatement->execute([':nom' => $_POST['nom']]);
+                $existing_username = $pdoStatement->fetchColumn();
+                if ($existing_username) {
+                    $error_message_pseudo = 'Le pseudo existe déjà';
+                }
+            }
+        }
 
-    header('Location: confirmation.html');
-    exit;
-  }
-  ?>
-  <div class="conteneur">
+        // Vérification du mot de passe
+        if (isset($_POST['mdp'])) {
+            $password_regex = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/'; 
+            if (!preg_match($password_regex, $_POST['mdp'])) {
+                $error_message_passe = 'Le mot de passe doit contenir au moins 8 caractères, une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial';
+            }
+        }
+
+        // Confirmation du mot de passe
+        if (isset($_POST['confirm_password'])) {
+            if ($_POST['confirm_password'] !== $_POST['mdp']) {
+                $error_message_passeconfirm = 'Les mots de passe ne correspondent pas';
+            }
+        }
+
+        // Insertion des données si aucune erreur n'a été rencontrée
+        if (!isset($error_message_mail) && 
+            !isset($error_message_pseudo) && 
+            !isset($error_message_passe) &&
+            !isset($error_message_passeconfirm)) {
+            
+            $pdoStatement = $pdo->prepare('INSERT INTO user (nom, mdp, score, mail) VALUES (:nom, :mdp, :score, :mail)');
+            $pdoStatement->execute([
+                ':nom' => $_POST['nom'],
+                ':mdp' => hash('sha256', $_POST['mdp']), // Hash du mot de passe (à adapter selon vos besoins)
+                ':score' => 0, // Valeur par défaut pour le score, à adapter si nécessaire
+                ':mail' => $_POST['mail']
+            ]);
+        }
+    }
+?>
+  <div class="container">
     <h1 class="title">Inscription </h1>
-    <form action="" method="post">
-      <label for="username" class="sousTitre">Nom d'utilisateur</label>
-      <input type="text" id="username" name="username" required>
-      <label for="email" class="sousTitre">Adresse e-mail</label>
-      <input type="email" id="email" name="email" required>
-      <label for="password" class="sousTitre">Mot de passe</label>
-      <input type="password" id="password" name="password" required>
+    <form action="#" method="POST">
+      <label for="username">Nom d'utilisateur</label>
+      <input type="text" id="nom" name="nom" required>
+      <label for="email">Adresse e-mail</label>
+      <input type="email" id="mail" name="mail" required>
+      <label for="password">Mot de passe</label>
+      <input type="password" id="mdp" name="mdp" required>
       <button type="submit" name="submit">Inscription</button>
     </form>
   </div>
